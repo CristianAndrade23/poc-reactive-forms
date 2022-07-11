02df65f56e8857f2567b05d1078cb434c82140f5
CristianAndrade23 <cristianandrade1903@gmail.com>
2022-07-10 21:47:48 -0300

    init form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormArray, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poc-reactive-forms';
+
+  form = this.fb.group({
+    name: '',
+    phone: '',
+    animals: this.fb.group({
+      dogs: false,
+      cat: false,
+      reptiles: false
+    }),
+    emails: this.fb.array([this.fb.group({
+      email: ''
+    })])
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  submit(): void{
+    console.log('form', this.form);
+  }
+
+  get emails(): FormArray {
+    return this.form.get('emails') as FormArray;
+  }
 }
